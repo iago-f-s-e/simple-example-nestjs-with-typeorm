1f094bc510fd3ed4abd5eade053fe792da987840
iago fagundes <iago.fagundes@outlook.com>
2021-12-02 23:11:35 -0300

    feat: setup database

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -1,6 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { DatabaseModule } from '@src/modules';
 import * as Settings from '../settings';
 
 export async function bootstrap() {
+  const app = await NestFactory.create(DatabaseModule);
+
+  await app.listen(Settings.PORT);
+
   console.log('******************************');
   console.log(`Server running on port: ${Settings.PORT}`);
   console.log('******************************');
---
+++ src/server/settings/database.ts
@@ -0,0 +1,5 @@
+export const DATABASE_HOST = process.env.DATABASE_HOST || 'localhost';
+export const DATABASE_PORT = Number(process.env.DATABASE_PORT) || 5432;
+export const DATABASE_NAME = process.env.DATABASE_NAME || 'postgres';
+export const DATABASE_USER = process.env.DATABASE_USER || 'postgres';
+export const DATABASE_PASS = process.env.DATABASE_PASS || 'postgres';
